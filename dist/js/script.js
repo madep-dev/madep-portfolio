4706585325d35b8406bedb51eccdd54ebb7901f8
madep <mahfudade@gmail.com>
2023-09-21 21:13:10 +0700

    Change design

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,32 @@
+// Particle js
+window.onload = function () {
+    Particles.init({
+        selector: ".background",
+        color: "#2563eb",
+        connectParticles: "true",
+        responsive: [
+            {
+                breakpoint: 1200,
+                options: {
+                    sizeVariations: "5",
+                    maxParticles: "40",
+                    minDistance: "80",
+                },
+            },
+        ],
+    });
+};
+
 // navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
 
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
-  }
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add("navbar-fixed");
+    } else {
+        header.classList.remove("navbar-fixed");
+    }
 };
 
 // hamburger
@@ -15,6 +34,6 @@ const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
-  theme: {
-    container: {
-      center: true,
-      padding: "16px",
+    content: ["index.html"],
+    theme: {
+        container: {
+            center: true,
+            padding: "16px",
+        },
+        extend: {
+            colors: {
+                primary: "#2563eb",
+                secondary: "#64748b",
+                dark: "#1e293b",
+            },
+            screens: {
+                "2xl": "1320px",
+            },
+            backgroundSize: {
+                "size-200": "200% 200%",
+            },
+            backgroundPosition: {
+                "pos-0": "0% 0%",
+                "pos-100": "100% 100%",
+            },
+        },
     },
-    extend: {
-      colors: {
-        primary: "#2563eb",
-        secondary: "#64748b",
-        dark: "#1e293b",
-      },
-      screens: {
-        "2xl": "1320px",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
